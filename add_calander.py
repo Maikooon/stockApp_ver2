f9b92c32e997d291648c0e0dc56183992a49c677
maikoon26 <101503385+Maikooon@users.noreply.github.com>
2023-11-10 14:57:45 +0900

    add func

--- add_calander.py
+++ add_calander.py
@@ -143,8 +143,8 @@ def main():
         print(event['id'])
 
 
-if __name__ == '__main__':
-    output = get_settleInfo(event.message.text)
-    result = transformStyle(output)
-    saveFile(result)
-    main()
+# if __name__ == '__main__':
+#     output = get_settleInfo(event.message.text)
+#     result = transformStyle(output)
+#     saveFile(result)
+#     main()
