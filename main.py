04afb90018a3607760e4ee429d9626a76bf4e298
maikoon26 <101503385+Maikooon@users.noreply.github.com>
2023-11-10 13:11:26 +0900

    add func

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ def callback():
 # botにメッセージを送ったときの処理
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    code = event.message.text
+    #code = event.message.text
     #get_settleInfo(code)
     line_bot_api.reply_message(
         event.reply_token,
