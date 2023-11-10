38eba4a06083dc089158b3d6bb6b49f51ffdcd73
maikoon26 <101503385+Maikooon@users.noreply.github.com>
2023-11-10 13:08:54 +0900

    add func

--- main.py
+++ main.py
@@ -84,7 +84,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     code = event.message.text
-    get_settleInfo(code)
+    #get_settleInfo(code)
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
