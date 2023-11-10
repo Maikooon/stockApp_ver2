7b36058706dea93488b3f8949a703369b2aad65c
maikoon26 <101503385+Maikooon@users.noreply.github.com>
2023-11-10 12:55:08 +0900

    add func

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ from PIL import Image
 from io import BytesIO
 import psycopg2
 import find_stock
+import requests
+from bs4 import BeautifulSoup
+import re
+import logging
 
 
 # サンプルコードの11~14行目を以下のように書き換え
@@ -30,6 +34,53 @@ header = {
     "Content_Type": "application/json",
     "Authorization": "Bearer " + LINE_CHANNEL_ACCESS_TOKEN
 }
+
+source = 'https://kabuyoho.ifis.co.jp/index.php?action=tp1&sa=report_top&bcode=' 
+
+
+# 決算日取得関数 
+def get_settleInfo(CODE):
+    # クローリング
+    try:
+        logging.debug('read web data cord = ' + CODE)  # logging
+        r = requests.get(source + CODE)
+        
+    except ValueError:
+        logging.debug('read web data ---> Exception Error')  # vlogging
+        return None, 'Exception error: access failed'
+    
+    # スクレイピング
+    soup = BeautifulSoup(r.content, "html.parser")
+    settleInfo = soup.find("div", class_="header_main").text
+    settleInfo = re.sub(r'[\n\t]+', ',', settleInfo)  # メタ文字の除去
+    settleInfo = re.sub(r'(^,)|(,$)', '', settleInfo)  # 行頭行末のカンマ除去
+    settleInfo = re.sub(r'[\xc2\xa0]', '', settleInfo)  # &nbsp(\xc2\xa0)問題の処置
+    logging.debug('settleInfo result = ' + settleInfo)  # logging
+
+    if not settleInfo:
+        settleInfo = 'not found'
+
+    return settleInfo
+
+
+def transformStyle(inputText):
+    parts = inputText.split(',')
+    date = parts[7].strip()
+    dateParts = date.split('/')
+    if len(dateParts) != 3:
+        return "日付の形式が正しくありません。"
+    formattedDate = f"{dateParts[0]}.{dateParts[1]}"
+    title = parts[1].strip()
+    quarter = parts[5].strip()
+    result = f"{formattedDate}\n{dateParts[2]} {title} {quarter}決算発表"
+    return result
+
+
+# if __name__ == '__main__':
+#     output = get_settleInfo(CODE)
+#     result = transformStyle(output)
+#     with open('output.txt', 'w') as f:
+#         f.write(result)
 
 
 @app.route("/")
@@ -60,8 +111,10 @@ def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
-    settle_info = get_settleInfo(CODE)
-    print("settle_info:", settle_info)
+    output = get_settleInfo(CODE)
+    result = transformStyle(output)
+    with open('output.txt', 'w') as f:
+        f.write(result)
     #print("返信完了!!\ntext:", event.message.text)
 
 
