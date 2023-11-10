2bc21c3c82d4c7aabc1f291f48ad42bb5f3e5b52
maikoon26 <101503385+Maikooon@users.noreply.github.com>
2023-11-10 12:56:16 +0900

    add func

--- main.py
+++ main.py
@@ -107,14 +107,14 @@ def callback():
 # botにメッセージを送ったときの処理
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    CODE = event.message.text
+    #CODE = event.message.text
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
-    output = get_settleInfo(CODE)
-    result = transformStyle(output)
-    with open('output.txt', 'w') as f:
-        f.write(result)
+    # output = get_settleInfo(CODE)
+    # result = transformStyle(output)
+    # with open('output.txt', 'w') as f:
+    #     f.write(result)
     #print("返信完了!!\ntext:", event.message.text)
 
 
