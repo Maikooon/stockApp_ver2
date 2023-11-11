4c34da4f70bea8515b47701cbd7e736a8bd3b1e5
maikoon26 <101503385+Maikooon@users.noreply.github.com>
2023-11-11 15:03:19 +0900

    add func

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from google.auth.transport.requests import Request
 from bs4 import BeautifulSoup
 import re
 import logging
-from add_calander import get_settleInfo, transformStyle, saveFile
+from add_calander import get_settleInfo, transformStyle, saveFile, main
 
 
 # サンプルコードの11~14行目を以下のように書き換え
@@ -97,21 +97,6 @@ source = 'https://kabuyoho.ifis.co.jp/index.php?action=tp1&sa=report_top&bcode='
 CODE = "6758"
 
 
-
-# できすと形式の変更
-# def transformStyle(inputText):
-#     parts = inputText.split(',')
-#     date = parts[7].strip()
-#     dateParts = date.split('/')
-#     if len(dateParts) != 3:
-#         return "日付の形式が正しくありません。"
-#     formattedDate = f"{dateParts[0]}.{dateParts[1]}"
-#     title = parts[1].strip()
-#     quarter = parts[5].strip()
-#     result = f"{formattedDate}\n{dateParts[2]} {title} {quarter}決算発表"
-#     return result
-
-
 # ファイルへの保存
 def saveFile(result):
     output = get_settleInfo(CODE)
@@ -133,74 +118,74 @@ def readSchedule():
     return lines1
 
 
-def main():
-    creds = None
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token: 
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
+# def main():
+#     creds = None
+#     if os.path.exists('token.pickle'):
+#         with open('token.pickle', 'rb') as token:
+#             creds = pickle.load(token)
+#     if not creds or not creds.valid:
+#         if creds and creds.expired and creds.refresh_token: 
+#             creds.refresh(Request())
+#         else:
+#             flow = InstalledAppFlow.from_client_secrets_file(
+#                 'credentials.json', SCOPES)
+#             creds = flow.run_local_server(port=0)
+#         with open('token.pickle', 'wb') as token:
+#             pickle.dump(creds, token)
 
-    service = build('calendar', 'v3', credentials=creds)
+#     service = build('calendar', 'v3', credentials=creds)
 
-    readSchedule()
-    print(readSchedule())
+#     readSchedule()
+#     print(readSchedule())
 
-    yearmon = readSchedule()[0]
-    a = yearmon.split(".")
-    year = int(a[0])
-    mon = int(a[1])
+#     yearmon = readSchedule()[0]
+#     a = yearmon.split(".")
+#     year = int(a[0])
+#     mon = int(a[1])
 
-    if mon == 1 or mon == 3 or mon == 5 or mon == 7 or mon == 8 or mon == 10 or mon == 12:
-        num_days = 31
-    elif mon == 2:
-        num_days = 28
-    else:
-        num_days = 30
-    for i in readSchedule():
-        s = i.split(' ')
-        if (len(s) == 1):
-            continue
+#     if mon == 1 or mon == 3 or mon == 5 or mon == 7 or mon == 8 or mon == 10 or mon == 12:
+#         num_days = 31
+#     elif mon == 2:
+#         num_days = 28
+#     else:
+#         num_days = 30
+#     for i in readSchedule():
+#         s = i.split(' ')
+#         if (len(s) == 1):
+#             continue
         
-        d_s = int(s[0])
-        d_e = int(s[0])
-        m_s = mon
-        m_e = mon
-        y_s = year
-        y_e = year
+#         d_s = int(s[0])
+#         d_e = int(s[0])
+#         m_s = mon
+#         m_e = mon
+#         y_s = year
+#         y_e = year
 
-        if (mon == 12 and d_e == 31):
-            y_e = year + 1
-        if (num_days == d_e):
-            d_e = 1
-            if mon == 12:
-                m_e = 1
-            else:
-                m_e = m_e + 1
+#         if (mon == 12 and d_e == 31):
+#             y_e = year + 1
+#         if (num_days == d_e):
+#             d_e = 1
+#             if mon == 12:
+#                 m_e = 1
+#             else:
+#                 m_e = m_e + 1
 
-        event = {
-            'summary': '{}'.format(s[1]),
-            'location': 'Japan',
-            'description': '{}'.format(s[2]),
-            'start': {
-                'date': '{}-{}-{}'.format(y_s, m_s, d_s),
-                'timeZone': 'Japan',
-            },
-            'end': {
-                'date': '{}-{}-{}'.format(y_e, m_e, d_e),
-                'timeZone': 'Japan',
-            },
-        }
-        event = service.events().insert(calendarId='maiko02626@gmail.com',
-                                        body=event).execute()
-        print(event['id'])
+#         event = {
+#             'summary': '{}'.format(s[1]),
+#             'location': 'Japan',
+#             'description': '{}'.format(s[2]),
+#             'start': {
+#                 'date': '{}-{}-{}'.format(y_s, m_s, d_s),
+#                 'timeZone': 'Japan',
+#             },
+#             'end': {
+#                 'date': '{}-{}-{}'.format(y_e, m_e, d_e),
+#                 'timeZone': 'Japan',
+#             },
+#         }
+#         event = service.events().insert(calendarId='maiko02626@gmail.com',
+#                                         body=event).execute()
+#         print(event['id'])
 
 
 
