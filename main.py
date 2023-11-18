0deb949afae60bf2afa4540b760be11b4b5014ba
maikoon26 <101503385+Maikooon@users.noreply.github.com>
2023-11-18 16:40:17 +0900

    add func

--- main.py
+++ main.py
@@ -31,10 +31,24 @@ def get_connection():
 def hello_world():
     return "hello world!"
 
+# # アプリにPOSTがあったときの処理
+# @app.route("/callback", methods=["POST"])
+# def callback():
+#     # get X-Line-Signature header value
+#     signature = request.headers["X-Line-Signature"]
+#     # get request body as text
+#     body = request.get_data(as_text=True)
+#     app.logger.info("Request body: " + body)
+#     # handle webhook body
+#     try:
+#         handler.handle(body, signature)
+#     except InvalidSignatureError:
+#         abort(400)
+#     return "OK"
 
 @app.route("/callback", methods=["POST"])
 def callback():
-    signature = request.headers.get("X-Line-Signature", "")
+    signature = request.headers.get("X-Line-Signature")
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
     try:
