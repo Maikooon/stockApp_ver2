dc7969121e582e339b790475fd104d94d91e96fa
maikoon26 <101503385+Maikooon@users.noreply.github.com>
2023-11-18 14:47:32 +0900

    add func

--- main.py
+++ main.py
@@ -1,48 +1,41 @@
 from flask import Flask, request, abort
-import requests, os
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage,  FollowEvent, UnfollowEvent
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, FollowEvent, UnfollowEvent
+import os
 import psycopg2
-from add_calander import get_settleInfo, transformStyle, saveFile, readSchedule, main
-
-
-# サンプルコードの11~14行目を以下のように書き換え
-LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
-LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
-DATABASE_URL = os.environ["DATABASE_URL"]
-HEROKU_APP_NAME = os.environ["HEROKU_APP_NAME"]
-
-# スクリプトのディレクトリを取得 パスを指定hしないとファイルが生成されなかった
-script_dir = os.path.dirname(os.path.abspath(__file__))
-output_path = os.path.join(script_dir, 'output.txt')   
+from add_calendar import get_settle_info, transform_style, save_file, read_schedule, main, output_path
 
 app = Flask(__name__)
-Heroku = "https://{}-d52dab965779.herokuapp.com/".format(HEROKU_APP_NAME)
 
+# Configurations
+LINE_CHANNEL_ACCESS_TOKEN = os.environ.get("LINE_CHANNEL_ACCESS_TOKEN", "")
+LINE_CHANNEL_SECRET = os.environ.get("LINE_CHANNEL_SECRET", "")
+DATABASE_URL = os.environ.get("DATABASE_URL", "")
+HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", "")
+CODE = "0"
+
+# LINE Bot API Setup
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
-header = {
-    "Content_Type": "application/json",
-    "Authorization": "Bearer " + LINE_CHANNEL_ACCESS_TOKEN
-}
-  
 
+# Database Connection Function
+def get_connection():
+    return psycopg2.connect(DATABASE_URL, sslmode="require")
+
+
+# Routes
 @app.route("/")
 def hello_world():
     return "hello world!"
 
 
-# アプリにPOSTがあったときの処理
 @app.route("/callback", methods=["POST"])
 def callback():
-    # get X-Line-Signature header value
-    signature = request.headers["X-Line-Signature"]
-    # get request body as text
+    signature = request.headers.get("X-Line-Signature", "")
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-    # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
@@ -50,12 +43,7 @@ def callback():
     return "OK"
 
 
-# データベース接続
-def get_connection():
-    return psycopg2.connect(DATABASE_URL, sslmode="require")
-
-
-# botがフォローされたときの処理
+# LINE Bot Events
 @handler.add(FollowEvent)
 def handle_follow(event):
     profile = line_bot_api.get_profile(event.source.user_id)
@@ -72,7 +60,6 @@ def handle_follow(event):
         print(db_check)
 
 
-# botがアンフォロー(ブロック)されたときの処理
 @handler.add(UnfollowEvent)
 def handle_unfollow(event):
     with get_connection() as conn:
@@ -82,50 +69,24 @@ def handle_unfollow(event):
     print("userIdの削除OK!!")
 
 
-# If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/calendar']
-
-
-# botにメッセージを送ったときの処理, これだとおk
-# @handler.add(MessageEvent, message=TextMessage)
-# def handle_message(event):
-#     received_text = event.message.text
-#     output = get_settleInfo(received_text) 
-#     result = transformStyle(output)
-#     saveFile(result, output_path)
-#     readSchedule()
-#     main()
-#     line_bot_api.reply_message(
-#             event.reply_token,
-#             TextSendMessage(text=event.message.text))
-#     print("返信完了!!\ntext:", event.message.text)
-
-CODE = "0"
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     global CODE
     received_text = event.message.text
 
-    # イベントのメッセージによって処理を変える
     if received_text.isdigit() and len(received_text) == 4:
-        # 4桁の数字の場合
-        print(received_text)
         CODE = received_text
         reply_text = "本当に追加しますか？"
     elif received_text == "":
-        # 何も入力されていない場合
         received_text = event.message.text
         reply_text = "証券コードを入力してください"
     elif received_text == "yes":
-        # "Yes" が入力された場合
-        print(CODE)
-        print("aaaaaaaa")
         if len(CODE) == 4:
-            output = get_settleInfo(CODE) 
-            result = transformStyle(output)
-            saveFile(result, output_path)
-            readSchedule()
-            main()  
+            output = get_settle_info(CODE) 
+            result = transform_style(output)
+            save_file(result, output_path)
+            read_schedule()
+            main()
             reply_text = "カレンダーに追加しました"
         else:
             reply_text = "証券コードが正しくありません。4桁の数字を入力してください。"
@@ -139,15 +100,14 @@ def handle_message(event):
 
     print("返信完了!!\ntext:", received_text)
 
- 
-# アプリの起動
-if  __name__ == "__main__":
-    # 初回のみデータベースのテーブル作成
+
+# Main
+if __name__ == "__main__":
     with get_connection() as conn:
         with conn.cursor() as cur:
             conn.autocommit = True
             cur.execute('CREATE TABLE IF NOT EXISTS users(user_id TEXT)')
-    
+
     port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
     handle_message()
